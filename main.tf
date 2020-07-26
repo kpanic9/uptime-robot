d977f808f5356a562973d0031dce53b80f6335f7
Namesh Sanjitha <tns.xiii@gmail.com>
2020-07-26 13:23:48 +1000

    Formatted code

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ resource "uptimerobot_monitor" "this" {
   friendly_name = "Google-Search"
   type          = "http"
   url           = var.url
-  interval      = 300       # with free account monitoring interval is 5 mins
+  interval      = 300 # with free account monitoring interval is 5 mins
 
-#   alert_contact {
-#     id = "${uptimerobot_alert_contact.slack.id}"
-#   }
+  #   alert_contact {
+  #     id = "${uptimerobot_alert_contact.slack.id}"
+  #   }
 
 }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "api_key" {
   description = "API key to access Uptime-Robot APIs"
-  type = string
+  type        = string
 }
 
 variable "url" {
   description = "URL to be monitored"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
